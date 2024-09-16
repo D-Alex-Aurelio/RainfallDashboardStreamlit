9cae95df3c9df7c57d179fda2737fa7d47fed3a6
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2024-09-16 14:57:05 +0800

    Initialization of Project

---
+++ sl1.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import plotly.express as px
+
+
+
+with st.sidebar:
+    st.write("This is a sidebar")
+    st.write("I am in a sidebar")
+    uploaded_file = st.file_uploader("Choose a CSV file")
+
+st.write("I am not in a sidebar")
+
+tab1, tab2, tab3 = st.tabs(["Yearly Analysis", "Monthly Analysis", "Daily Analysis"])
+
+with tab1:
+    st.header("This is for the Yearly Analysis of Rainfall Data")
+
+with tab2:
+    st.header("This is for the Monthly Analysis of Rainfall Data")
+
+with tab3:
+    st.header("This is for the Daily Analysis of Rainfall Data")
