1d81f8cc6f2be3bb26ccfc56a4da1f61bea8e3f1
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2024-09-17 14:50:31 +0800

    Added Daily Analysis

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,14 +38,18 @@ df_year = df.groupby(by=df.index.year).sum()/1000
 
 
 # convert mm to cm for monthly analysis
-df_month = df
-df_month['RAINFALL'] = df_month['RAINFALL']/10
+df_month = pd.DataFrame(df)
+df_month["RAINFALL"] = df_month["RAINFALL"]/10
 df_month = pd.pivot_table(df_month,
                           values="RAINFALL",
-                          index=[df.index.month],
-                          columns=[df.index.year],
+                          index=[df_month.index.month],
+                          columns=[df_month.index.year],
                           aggfunc="sum")
 
+# adding position to daily rainfall
+df_rf = pd.DataFrame({"RAINFALL":df["RAINFALL"].values})
+df_rf = df_rf.sort_values(by="RAINFALL", ignore_index=True)
+df_rf["POSITION"] = (list(range(1,df_rf.count().iloc[0]+1,1)))/(df_rf.count().iloc[0]+1)
 
 
 tab1, tab2, tab3 = st.tabs(["Yearly Analysis", "Monthly Analysis", "Daily Analysis"])
@@ -54,18 +58,38 @@ with tab1:
     st.header("Yearly Rainfall Trend and Statistics")
     yearly_plot_1 = px.bar(df_year)
     st.plotly_chart(yearly_plot_1)
-    st.write(f"Max: {df_year.max().iloc[0]} meters")
-    st.write(f"Min: {df_year.min().iloc[0]} meters")
-    st.write(f"Median: {df_year.median().iloc[0]} meters")
+    st.write(df_year.describe())
 
 with tab2:
     st.header("Monthly Rainfall Trend")
     monthly_plot_1 = px.imshow(df_month, color_continuous_scale="Blues")
     st.plotly_chart(monthly_plot_1)
+
     st.header("Monthly Rainfall Statistics")
     monthly_plot_2 = px.box(df_month)
     st.plotly_chart(monthly_plot_2)
+    st.write(df_month.describe())
 
 with tab3:
-    st.header("This is for the Daily Analysis of Rainfall Data")
-    st.write(df)
+    st.header("Daily Rainfall Trend")
+
+    daily_plot_1 = px.line(df)
+    st.plotly_chart(daily_plot_1)
+
+    st.header("Daily Rainfall Statistics")
+
+    st.write(df_rf["RAINFALL"].describe())
+
+    daily_plot_2 = px.line(df_rf,
+                           x="RAINFALL",
+                           y="POSITION")
+    st.plotly_chart(daily_plot_2)
+
+    st.write("Percentiles")
+    rf_percs = [0,10,20,30,40,50,60,70,80,90,95,99]
+    rf_vals = []
+    for rf_perc in rf_percs:
+        rf_vals.append(df_rf[df_rf["POSITION"]>rf_perc/100]["RAINFALL"].iloc[0])
+    df_perc = pd.DataFrame({"PERCENTILE": rf_percs,"RAINFALL": rf_vals})
+    st.write(df_perc)
+
