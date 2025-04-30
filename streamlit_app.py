71623564869694164ef5dedfedd83726a48aa929
D-Alex-Aurelio <162036296+D-Alex-Aurelio@users.noreply.github.com>
2025-04-30 17:42:05 +0800

    Added raw data tab with filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,90 @@
 from operator import index
 
+from pandas.api.types import (
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
+def filter_dataframe(df_f: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df_f (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df_f
+
+    df_f = df_f.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df_f.columns:
+        if is_object_dtype(df_f[col]):
+            try:
+                df_f[col] = pd.to_datetime(df_f[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df_f[col]):
+            df_f[col] = df_f[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df_f.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+
+            if isinstance(df_f[column],pd.CategoricalDtype) or df_f[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df_f[column].unique(),
+                    default=list(df_f[column].unique()),
+                )
+                df_f = df_f[df_f[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df_f[column]):
+                _min = float(df_f[column].min())
+                _max = float(df_f[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df_f = df_f[df_f[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df_f[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df_f[column].min(),
+                        df_f[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df_f = df_f.loc[df_f[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df_f = df_f[df_f[column].astype(str).str.contains(user_text_input)]
+
+    return df_f
 
 df = pd.read_csv("data/default data.csv")
 
@@ -33,9 +115,9 @@ df = pd.DataFrame(
         "RAINFALL": [x if x >= 0 else 0 for x in df["RAINFALL"].values]
     }
 )
+
 # convert mm to m for yearly analysis
 df_year = df.groupby(by=df.index.year).sum()/1000
-
 
 # convert mm to cm for monthly analysis
 df_month = pd.DataFrame(df)
@@ -51,11 +133,17 @@ df_rf = pd.DataFrame({"RAINFALL":df["RAINFALL"].values})
 df_rf = df_rf.sort_values(by="RAINFALL", ignore_index=True)
 df_rf["POSITION"] = (list(range(1,df_rf.count().iloc[0]+1,1)))/(df_rf.count().iloc[0]+1)
 
+
+# dashboard layout
 st.title("Rainfall Analysis Dashboard")
 
-tab1, tab2, tab3 = st.tabs(["Yearly Analysis", "Monthly Analysis", "Daily Analysis"])
+tab1, tab2, tab3, tab4 = st.tabs(["Raw Data","Yearly Analysis", "Monthly Analysis", "Daily Analysis"])
 
 with tab1:
+    st.header("Raw Data")
+    st.dataframe(filter_dataframe(df),use_container_width=True)
+
+with tab2:
     st.header("Yearly Rainfall Trend and Statistics")
     yearly_plot_1 = px.bar(df_year,
                            x=df_year.index,
@@ -67,7 +155,7 @@ with tab1:
     st.plotly_chart(yearly_plot_1)
     st.write(df_year.describe())
 
-with tab2:
+with tab3:
     st.header("Monthly Rainfall Trend")
     monthly_plot_1 = px.imshow(df_month,
                                color_continuous_scale="Blues",
@@ -82,7 +170,7 @@ with tab2:
     st.plotly_chart(monthly_plot_2)
     st.write(df_month.describe())
 
-with tab3:
+with tab4:
     st.header("Daily Rainfall Trend")
     daily_plot_1 = px.line(df,
                            y="RAINFALL")
